8f0e7187e80c6f45c7b77bccb296fb8cb212c3b9
fwcd <fwcdmail@gmail.com>
2022-04-10 04:08:31 +0200

    Embed migrations

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,29 @@
+#[macro_use]
+extern crate diesel_migrations;
+
 mod routes;
 
-use std::io;
+use std::{io, env};
 
 use actix_web::{HttpServer, App};
+use diesel::{PgConnection, Connection};
+use diesel_migrations::embed_migrations;
+use dotenv::dotenv;
+
+embed_migrations!();
 
 #[actix_web::main]
 async fn main() -> io::Result<()> {
+    dotenv().ok();
+
+    // Connect to database
+    let db_url = env::var("DATABASE_URL").expect("The DATABASE_URL must be set");
+    let connection = PgConnection::establish(&db_url).expect(&format!("Error connecting to {}", db_url));
+
+    // Run database migrations
+    embedded_migrations::run(&connection).expect("Could not run migrations");
+
+    // Start server
     HttpServer::new(|| {
         App::new()
             .configure(routes::api_config)
