e727037dfa8be7d360c9c7acb18d3e660092f007
fwcd <fwcdmail@gmail.com>
2022-04-15 02:32:50 +0200

    Update models

--- src/models.rs
+++ src/models.rs
@@ -1,3 +1,5 @@
+use std::time::SystemTime;
+
 use serde::{Serialize, Deserialize};
 
 use crate::schema::*;
@@ -6,25 +8,69 @@ use crate::schema::*;
 pub struct Artist {
     pub id: i32,
     pub name: String,
+    pub cover_art_id: Option<i32>,
+    pub last_modified_at: SystemTime,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize, Queryable)]
 pub struct Album {
     pub id: i32,
     pub title: String,
+    pub cover_art_id: Option<i32>,
+    pub last_modified_at: SystemTime,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize, Queryable)]
 pub struct Track {
     pub id: i32,
     pub title: String,
-    pub album_id: Option<i32>,
+    pub year: Option<String>,
+    pub comment: Option<String>,
+    pub duration_ms: Option<i32>,
+    pub sample_rate: Option<i32>,
+    pub channels: Option<i32>,
+    pub bpm: Option<i32>,
+    pub times_played: usize,
+    pub rating: Option<i32>,
+    pub key: Option<String>,
+    pub color: Option<i32>,
+    pub added_at: SystemTime,
+    pub last_modified_at: SystemTime,
+    pub last_played_at: Option<SystemTime>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, Queryable)]
+pub struct Genre {
+    pub id: i32,
+    pub name: String,
+    pub last_modified_at: SystemTime,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, Queryable)]
+pub struct Resource {
+    pub id: i32,
+    pub location: String,
+    pub is_local: bool,
+    pub kind: i32, // TODO: Strongly typed as enum?
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize, Queryable)]
 pub struct TrackArtist {
     pub track_id: i32,
     pub artist_id: i32,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, Queryable)]
+pub struct TrackAlbum {
+    pub track_id: i32,
+    pub album_id: i32,
+    pub position: i32,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, Queryable)]
+pub struct TrackGenre {
+    pub track_id: i32,
+    pub genre_id: i32,
 }
 
 #[derive(Debug, Clone, Insertable)]
@@ -43,5 +89,4 @@ pub struct NewAlbum<'a> {
 #[table_name = "track"]
 pub struct NewTrack<'a> {
     pub title: &'a str,
-    pub album_id: Option<i32>,
 }
