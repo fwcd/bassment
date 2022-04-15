97d37c171efb55279d4ac2afc2cb80043d5e2fbe
fwcd <fwcdmail@gmail.com>
2022-04-15 03:37:50 +0200

    Merge crate and playlist schema for now
    Crate seems to be a reserved keyword that cannot be used for table names
    (see https://github.com/diesel-rs/diesel/issues/3133).

--- migrations/2022-04-15-004824_add_playlist/down.sql
+++ migrations/2022-04-15-004824_add_playlist/down.sql
@@ -1,6 +1,4 @@
 -- Drop crates and playlists.
 
-DROP TABLE crate_track;
 DROP TABLE playlist_track;
-DROP TABLE crate;
 DROP TABLE playlist;
---
+++ migrations/2022-04-15-004824_add_playlist/up.sql
@@ -0,0 +1,21 @@
+-- Add crates and playlists.
+
+CREATE TABLE playlist (
+    id SERIAL PRIMARY KEY,
+    name TEXT DEFAULT 'New Playlist' NOT NULL,
+    kind INT DEFAULT 0 NOT NULL, -- 0 => default,
+                                 -- 1 => folder,
+                                 -- 2 => crate,
+                                 -- 3 => set log
+    cover_art_id INT REFERENCES resource(id),
+    parent_id INT REFERENCES playlist(id),
+    position INT NOT NULL -- starts from 0, indexed within the parent folder
+                          -- (i.e. the first child is always 0)
+);
+
+CREATE TABLE playlist_track (
+    playlist_id INT REFERENCES playlist(id),
+    track_id INT REFERENCES track(id),
+    track_number INT,
+    CONSTRAINT playlist_track_pkey PRIMARY KEY (playlist_id, track_id)
+);
