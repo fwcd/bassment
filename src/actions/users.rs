ebc27c03248a1d1f3562122d19fc031edb7bca23
fwcd <fwcdmail@gmail.com>
2022-04-16 05:46:46 +0200

    Automatically generate a root user if not exists

--- src/actions/users.rs
+++ src/actions/users.rs
@@ -1,6 +1,7 @@
 use diesel::{RunQueryDsl, QueryDsl, ExpressionMethods};
+use passwords::PasswordGenerator;
 
-use crate::{models::{User, NewUser}, error::Result, db::DbConn};
+use crate::{models::{User, NewUser}, error::{Result, Error}, db::DbConn};
 use crate::schema::users::dsl::*;
 
 /// Fetches all users from the database.
@@ -14,9 +15,22 @@ pub fn by_username(filtered_name: &str, conn: &DbConn) -> Result<User> {
         .get_result(conn)?)
 }
 
+/// Fetches the root user.
+pub fn root(conn: &DbConn) -> Result<User> {
+    by_username("root", conn)
+}
+
 /// Inserts a user into the database.
 pub fn insert(new_user: &NewUser, conn: &DbConn) -> Result<User> {
     Ok(diesel::insert_into(users)
         .values(new_user)
         .get_result(conn)?)
+}
+
+/// Creates a new root user and returns the password.
+pub fn generate_root(conn: &DbConn) -> Result<(String, User)> {
+    let password = PasswordGenerator::new().length(48).generate_one()
+        .map_err(|e| Error::Internal(format!("Could not generate root password: {}", e)))?;
+    let user = insert(&NewUser::new("root", &password)?, conn)?;
+    Ok((password, user))
 }
--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ use actix_web::{web, HttpServer, App};
 use diesel::{PgConnection, r2d2::{ConnectionManager, Pool}};
 use diesel_migrations::embed_migrations;
 use dotenv::dotenv;
-use tracing::Level;
+use tracing::{Level, info};
 use tracing_actix_web::TracingLogger;
 use tracing_subscriber::fmt::format::FmtSpan;
 
@@ -44,6 +44,15 @@ async fn main() -> io::Result<()> {
 
     // Run database migrations
     embedded_migrations::run(&pool.get().unwrap()).expect("Could not run migrations");
+
+    // Generate root user if not exists
+    {
+        let conn = pool.get().expect("Could not fetch connection for checking root user");
+        if actions::users::root(&conn).is_err() {
+            let (password, _) = actions::users::generate_root(&conn).expect("Could not generate root user");
+            info!("// ROOT-PASSWORD: {}", password)
+        }
+    }
 
     // Start server
     HttpServer::new(move || {
