2d105ac8249877c7abad43a851a5e4995fbc7f84
fwcd <fwcdmail@gmail.com>
2022-05-02 15:27:08 +0200

    Set up basic react-native-web infrastructure
    See https://dev.to/shivams136/create-react-native-app-for-android-ios-and-web-without-expo-48lc

--- frontend/index.web.js
+++ frontend/index.web.js
@@ -3,3 +3,6 @@ import App from './App';
 import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+AppRegistry.runApplication(appName, {
+  rootTag: document.getElementById('app')
+});
