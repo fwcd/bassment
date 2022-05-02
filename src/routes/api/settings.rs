4251c73667d85373ccda0b56e603d7b166be15f1
fwcd <fwcdmail@gmail.com>
2022-05-02 18:52:58 +0200

    Fix typo

--- src/routes/api/settings.rs
+++ src/routes/api/settings.rs
@@ -6,15 +6,15 @@ use crate::{actions::settings, db::DbPool, error::Result, middleware::auth::auth
 #[get("")]
 async fn get(pool: web::Data<DbPool>) -> Result<impl Responder> {
     let conn = pool.get()?;
-    let users = web::block(move || settings::get(&conn)).await??;
-    Ok(web::Json(users))
+    let settings = web::block(move || settings::get(&conn)).await??;
+    Ok(web::Json(settings))
 }
 
 #[patch("")]
 async fn patch(pool: web::Data<DbPool>, settings: web::Json<UpdateSettings>) -> Result<impl Responder> {
     let conn = pool.get()?;
-    let users = web::block(move || settings::update(&settings, &conn)).await??;
-    Ok(web::Json(users))
+    let new_settings = web::block(move || settings::update(&settings, &conn)).await??;
+    Ok(web::Json(new_settings))
 }
 
 pub fn config(cfg: &mut web::ServiceConfig) {
