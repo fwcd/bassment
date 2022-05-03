33aef8d8ddd452b86961a763425990134ed8f4e0
fwcd <fwcdmail@gmail.com>
2022-05-03 23:46:35 +0200

    Add url-loader for image resources
    See https://necolas.github.io/react-native-web/docs/multi-platform/

--- frontend/web/webpack.rules.js
+++ frontend/web/webpack.rules.js
@@ -4,9 +4,19 @@ module.exports = [
     exclude: /node_modules/,
     loader: 'ts-loader',
   },
+  {
+    test: /\.(gif|jpe?g|png|svg)$/,
+    use: {
+      loader: 'url-loader',
+      options: {
+        name: '[name].[ext]',
+        esModule: false,
+      },
+    },
+  },
   // {
   //   enforce: 'pre',
   //   test: /\.js$/,
   //   loader: 'source-map-loader',
   // },
-]
+];
