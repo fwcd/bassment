f339566fd5fc46d6b7104fbeb69ea1ad726f214a
fwcd <fwcdmail@gmail.com>
2022-05-03 21:44:21 +0200

    Only import react-native-gesture-handler on mobile

--- frontend/src/index.web.ts
+++ frontend/src/index.web.ts
@@ -1,5 +1,8 @@
-import 'react-native-gesture-handler';
 import { AppRegistry } from 'react-native';
 import App from './App';
 
 AppRegistry.registerComponent('Bassment', () => App);
+
+AppRegistry.runApplication('Bassment', {
+  rootTag: document.getElementById('app'),
+});
