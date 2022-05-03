c21e218fa17410aa3a63bc0091492cf4c5268f63
fwcd <fwcdmail@gmail.com>
2022-05-04 00:00:42 +0200

    Use babel-loader for web

--- frontend/index.ts
+++ frontend/index.ts
@@ -1,6 +1,7 @@
 import { AppRegistry, Platform } from 'react-native';
 import { App } from './App';
-import { name as appName } from './app.json';
+
+const appName = 'Bassment';
 
 AppRegistry.registerComponent(appName, () => App);
 
--- frontend/web/webpack.config.js
+++ frontend/web/webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const RULES = require('./webpack.rules');
 const rootDir = path.join(__dirname, '..');
 const webpackEnv = process.env.NODE_ENV || 'development';
 
@@ -15,7 +14,47 @@ module.exports = {
   },
   devtool: 'source-map',
   module: {
-    rules: RULES,
+    rules: [
+      // {
+      //   test: /\.(tsx|ts|jsx|js|mjs)$/,
+      //   exclude: /node_modules/,
+      //   use: {
+      //     loader: 'ts-loader',
+      //   },
+      // },
+      {
+        test: /\.(j|t)sx?$/,
+        include: [
+          path.resolve(rootDir, 'app.json'),
+          path.resolve(rootDir, 'App.tsx'),
+          path.resolve(rootDir, 'src'),
+          path.resolve(rootDir, 'node_modules', 'react-native-gesture-handler'),
+          path.resolve(rootDir, 'node_modules', 'react-native-reanimated'),
+        ],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['module:metro-react-native-babel-preset'],
+            plugins: ['react-native-web'],
+          },
+        },
+      },
+      {
+        test: /\.(gif|jpe?g|png|svg)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            name: '[name].[ext]',
+            esModule: false,
+          },
+        },
+      },
+      // {
+      //   enforce: 'pre',
+      //   test: /\.js$/,
+      //   loader: 'source-map-loader',
+      // },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
