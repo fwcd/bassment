c734ba3be288b2a0e5bd45a188a4654aceb4f127
fwcd <fwcdmail@gmail.com>
2022-05-04 05:16:06 +0200

    [frontend] Move drawer into AppContainer

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,49 +1,13 @@
 import React from 'react';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
-import { Platform, Text, useWindowDimensions } from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
-
-const Drawer = createDrawerNavigator();
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from 'react-native';
+import { AppContainer } from '@bassment/AppContainer';
 
 export function App() {
-  const dimensions = useWindowDimensions();
   return (
-    <NavigationContainer>
-      <Drawer.Navigator
-        initialRouteName="Test"
-        screenOptions={{
-          drawerType:
-            Platform.OS === 'web' || dimensions.width >= 600
-              ? 'permanent'
-              : 'slide',
-          drawerStyle: {
-            minWidth: 200,
-          },
-        }}>
-        <Drawer.Screen
-          name="Home"
-          options={{
-            drawerIcon: props => (
-              <Icon name="home-outline" size={props.size} color={props.color} />
-            ),
-          }}
-          component={() => <Text>Home</Text>}
-        />
-        <Drawer.Screen
-          name="Test"
-          options={{
-            drawerIcon: props => (
-              <Icon
-                name="albums-outline"
-                size={props.size}
-                color={props.color}
-              />
-            ),
-          }}
-          component={() => <Text>Test 123</Text>}
-        />
-      </Drawer.Navigator>
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <StatusBar />
+      <AppContainer />
+    </SafeAreaProvider>
   );
 }
---
+++ frontend/src/AppContainer.tsx
@@ -0,0 +1,49 @@
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import { Platform, Text, useWindowDimensions } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+const Drawer = createDrawerNavigator();
+
+export function AppContainer() {
+  const dimensions = useWindowDimensions();
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator
+        initialRouteName="Test"
+        screenOptions={{
+          drawerType:
+            Platform.OS === 'web' || dimensions.width >= 600
+              ? 'permanent'
+              : 'slide',
+          drawerStyle: {
+            minWidth: 200,
+          },
+        }}>
+        <Drawer.Screen
+          name="Home"
+          options={{
+            drawerIcon: props => (
+              <Icon name="home-outline" size={props.size} color={props.color} />
+            ),
+          }}
+          component={() => <Text>Home</Text>}
+        />
+        <Drawer.Screen
+          name="Test"
+          options={{
+            drawerIcon: props => (
+              <Icon
+                name="albums-outline"
+                size={props.size}
+                color={props.color}
+              />
+            ),
+          }}
+          component={() => <Text>Test 123</Text>}
+        />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
