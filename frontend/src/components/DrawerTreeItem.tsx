9e6537464b90be88826094b5748d83385811810e
fwcd <fwcdmail@gmail.com>
2022-05-04 13:30:56 +0200

    [frontend] Add chevrons to DrawerTreeItems

--- frontend/src/components/DrawerTreeItem.tsx
+++ frontend/src/components/DrawerTreeItem.tsx
@@ -1,6 +1,7 @@
 import { DrawerItem } from '@react-navigation/drawer';
 import React, { ReactNode, useState } from 'react';
-import { View } from 'react-native';
+import { View, ViewStyle } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 interface DrawerTreeItemProps {
   label: string;
@@ -12,11 +13,33 @@ interface DrawerTreeItemProps {
 
 export function DrawerTreeItem(props: DrawerTreeItemProps) {
   const [isExpanded, setExpanded] = useState(false);
+  const chevronStyles: ViewStyle = {
+    width: 25,
+    paddingEnd: 30,
+  };
   return (
     <>
       <DrawerItem
         label={props.label}
-        icon={props.icon}
+        icon={iconProps => (
+          <>
+            {props.children ? (
+              <Icon
+                name={
+                  isExpanded
+                    ? 'chevron-down-outline'
+                    : 'chevron-forward-outline'
+                }
+                size={iconProps.size}
+                color={iconProps.color}
+                style={chevronStyles}
+              />
+            ) : (
+              <View style={chevronStyles} />
+            )}
+            {props.icon ? props.icon(iconProps) : []}
+          </>
+        )}
         focused={props.focused}
         onPress={() =>
           props.onPress ? props.onPress() : setExpanded(!isExpanded)
