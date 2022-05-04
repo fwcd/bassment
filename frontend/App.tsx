0234cdc1f03098fd015b7a4f3c07314566d46ecf
fwcd <fwcdmail@gmail.com>
2022-05-04 02:28:33 +0200

    Use permanent drawer if wide enough

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
-import { Text } from 'react-native';
+import { Text, useWindowDimensions } from 'react-native';
 
 const Drawer = createDrawerNavigator();
 
 export function App() {
+  const dimensions = useWindowDimensions();
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
+      <Drawer.Navigator
+        initialRouteName="Test"
+        screenOptions={{
+          drawerType: dimensions.width >= 600 ? 'permanent' : 'slide',
+        }}>
         <Drawer.Screen name="Home" component={() => <Text>Home</Text>} />
-        <Drawer.Screen name="Test" component={() => <Text>Test</Text>} />
+        <Drawer.Screen name="Test" component={() => <Text>Test 123</Text>} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
