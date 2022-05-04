bd080628b5d1e113324ed1cbaff574b8073c28f5
fwcd <fwcdmail@gmail.com>
2022-05-04 03:24:41 +0200

    [frontend] Include icon font on web

--- frontend/index.ts
+++ frontend/index.ts
@@ -1,13 +1,6 @@
-import { AppRegistry, Platform } from 'react-native';
+import { AppRegistry } from 'react-native';
 import { App } from './App';
 
+// Start app
 const appName = 'Bassment';
-
 AppRegistry.registerComponent(appName, () => App);
-
-if (Platform.OS === 'web') {
-  AppRegistry.runApplication(appName, {
-    initialProps: {},
-    rootTag: document.getElementById('app-root'),
-  });
-}
--- frontend/index.web.ts
+++ frontend/index.web.ts
@@ -1,10 +1,23 @@
 import { AppRegistry } from 'react-native';
 import { App } from './App';
 
+// Use prebuilt version of icons
+// Source: https://stackoverflow.com/a/64133374
+// @ts-ignore
+import iconFont from 'react-native-vector-icons/Fonts/Ionicons.ttf';
+
+// Add CSS font-face for icons
+const iconFontStyles = `@font-face {
+  src: url(${iconFont});
+  font-family: Ionicons;
+}`;
+const style = document.createElement('style');
+style.innerText = iconFontStyles;
+document.head.appendChild(style);
+
+// Start app
 const appName = 'Bassment';
-
 AppRegistry.registerComponent(appName, () => App);
-
 AppRegistry.runApplication(appName, {
   initialProps: {},
   rootTag: document.getElementById('app-root'),
