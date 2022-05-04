edfcafd820910d63d15a5a1e2aad5c3c3d5da603
fwcd <fwcdmail@gmail.com>
2022-05-04 13:05:54 +0200

    [frontend] Render drawer items manually

--- frontend/src/AppSidebar.tsx
+++ frontend/src/AppSidebar.tsx
@@ -3,15 +3,36 @@ import { SearchBar } from '@bassment/components/SearchBar';
 import {
   DrawerContentComponentProps,
   DrawerContentScrollView,
-  DrawerItemList,
+  DrawerItem,
 } from '@react-navigation/drawer';
 import React from 'react';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export function AppSidebar(props: DrawerContentComponentProps) {
+  const routeName = props.state.routeNames[props.state.index];
   return (
     <DrawerContentScrollView>
       <SearchBar />
-      <DrawerItemList {...props} />
+      <DrawerItem
+        label="Tracks"
+        icon={({ size, color }) => (
+          <Icon name="musical-notes-outline" size={size} color={color} />
+        )}
+        focused={routeName === 'Tracks'}
+        onPress={() => {
+          props.navigation.navigate('Tracks');
+        }}
+      />
+      <DrawerItem
+        label="Albums"
+        icon={({ size, color }) => (
+          <Icon name="albums-outline" size={size} color={color} />
+        )}
+        focused={routeName === 'Albums'}
+        onPress={() => {
+          props.navigation.navigate('Albums');
+        }}
+      />
       <HorizontalDivider />
     </DrawerContentScrollView>
   );
