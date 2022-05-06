5b305e8ea9ed98bd38923a02622b245e083c1854
fwcd <fwcdmail@gmail.com>
2022-05-06 13:51:42 +0200

    Remove bogus hook

--- frontend/src/hooks/useRandom/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './useRandom.hook';
--- frontend/src/hooks/useRandom/useRandom.hook.tsx
+++
@@ -1,7 +0,0 @@
-import { RandomContext } from '@bassment/contexts/random';
-import { useContext } from 'react';
-
-/**
- * Hook for accessing RandomContext.
- */
-export const useRandom = () => useContext(RandomContext);
