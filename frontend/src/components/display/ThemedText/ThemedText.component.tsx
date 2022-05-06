d17d7628141f1bf298df83e0b346cc0be9603c81
fwcd <fwcdmail@gmail.com>
2022-05-06 04:55:00 +0200

    Add selectable prop to ThemedText

--- frontend/src/components/display/ThemedText/ThemedText.component.tsx
+++ frontend/src/components/display/ThemedText/ThemedText.component.tsx
@@ -5,16 +5,19 @@ import { StyleProp, Text, TextStyle } from 'react-native';
 interface ThemedTextProps {
   children: ReactNode;
   style?: StyleProp<TextStyle>;
+  selectable?: boolean;
 }
 
 /**
  * A wrapper around the built-in `Text` component that respects
  * the currently used theme (light or dark).
  */
-export function ThemedText({ children, style }: ThemedTextProps) {
+export function ThemedText({ children, style, selectable }: ThemedTextProps) {
   const styles = useThemedTextStyles();
   return (
-    <Text style={[styles.themedText, ...(style ? [style] : [])]}>
+    <Text
+      style={[styles.themedText, ...(style ? [style] : [])]}
+      selectable={selectable}>
       {children}
     </Text>
   );
