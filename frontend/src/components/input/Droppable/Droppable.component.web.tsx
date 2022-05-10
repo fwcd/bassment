76818132055d1efa3e82d51c043400fa982853d7
fwcd <fwcdmail@gmail.com>
2022-05-10 19:24:37 +0200

    Pass drop values to drag events

--- frontend/src/components/input/Droppable/Droppable.component.web.tsx
+++ frontend/src/components/input/Droppable/Droppable.component.web.tsx
@@ -1,10 +1,41 @@
 import { DroppableProps } from '@bassment/components/input/Droppable/Droppable.props';
-import { DropFile } from '@bassment/models/Drop';
+import { Drop, DropFile } from '@bassment/models/Drop';
 import React, { DragEventHandler, useCallback } from 'react';
 
+/** Converts a data transfer (from the DOM API) to a Drop (from our model). */
+function dropOf(transfer: DataTransfer): Drop | undefined {
+  if (transfer.files.length > 0) {
+    // Create a file drop
+    const files: DropFile[] = [];
+    for (let i = 0; i < transfer.files.length; i++) {
+      const file = transfer.files[i];
+      files.push({
+        name: file.name,
+        size: file.size,
+        content: file.arrayBuffer,
+      });
+    }
+    return { type: 'file', files };
+  } else if (transfer.items.length > 0) {
+    // Create an item drop
+    const items: any[] = [];
+    for (let i = 0; i < transfer.items.length; i++) {
+      items.push(transfer.items[i]);
+    }
+    return { type: 'item', items };
+  }
+  return undefined;
+}
+
 export function Droppable({ onDrag, onDrop, children }: DroppableProps) {
-  const onDragIn = useCallback(() => onDrag(true), [onDrag]);
-  const onDragOut = useCallback(() => onDrag(false), [onDrag]);
+  const onDragIn: DragEventHandler = useCallback(
+    event => onDrag(true, dropOf(event.dataTransfer)),
+    [onDrag],
+  );
+  const onDragOut: DragEventHandler = useCallback(
+    event => onDrag(false, dropOf(event.dataTransfer)),
+    [onDrag],
+  );
 
   const onDragOver: DragEventHandler = useCallback(event => {
     // Prevent file from being opened
@@ -20,32 +51,12 @@ export function Droppable({ onDrag, onDrop, children }: DroppableProps) {
       event.preventDefault();
 
       // Extract the transferred data and process it
-      const transfer = event.dataTransfer;
-
-      if (transfer.files.length > 0) {
-        const files: DropFile[] = [];
-
-        for (let i = 0; i < transfer.files.length; i++) {
-          const file = transfer.files[i];
-          files.push({
-            name: file.name,
-            size: file.size,
-            content: file.arrayBuffer,
-          });
-        }
-
-        onDrop({ type: 'file', files });
-      } else if (transfer.items.length > 0) {
-        const items: any[] = [];
-
-        for (let i = 0; i < transfer.items.length; i++) {
-          items.push(transfer.items[i]);
-        }
-
-        onDrop({ type: 'item', items });
+      const drop = dropOf(event.dataTransfer);
+      if (drop) {
+        onDrop(drop);
       }
     },
-    [onDrop],
+    [onDrag, onDrop],
   );
 
   return (
--- frontend/src/components/input/Droppable/Droppable.props.ts
+++ frontend/src/components/input/Droppable/Droppable.props.ts
@@ -2,7 +2,7 @@ import { Drop } from '@bassment/models/Drop';
 import { ReactNode } from 'react';
 
 export interface DroppableProps {
-  onDrag: (hovering: boolean) => void;
+  onDrag: (hovering: boolean, value?: Drop) => void;
   onDrop: (value: Drop) => void;
   // TODO: onDrop accepting some value
   children: ReactNode;
