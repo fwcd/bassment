d73b131668b7f894d211d119e130158b60730741
fwcd <fwcdmail@gmail.com>
2022-05-10 03:36:03 +0200

    Remove window dimensions dependency
    This stops our app from rerendering entirely when the window, addressing
    at least part of #44.

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,17 +1,20 @@
 import { AppContainer } from '@bassment/AppContainer';
+import { envConstants } from '@bassment/constants/env';
 import { ApiContextProvider } from '@bassment/contexts/Api';
 import { AudioPlayerContextProvider } from '@bassment/contexts/AudioPlayer';
 import { AuthContextProvider } from '@bassment/contexts/Auth';
 import { SearchContextProvider } from '@bassment/contexts/Search';
 import React from 'react';
-import { StatusBar, useWindowDimensions, View } from 'react-native';
+import { StatusBar, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export function App() {
-  const dimensions = useWindowDimensions();
   return (
     <SafeAreaProvider>
-      <View style={{ height: dimensions.height }}>
+      <View
+        style={{
+          height: envConstants.platform.os === 'web' ? '100vh' : '100%',
+        }}>
         <StatusBar />
         <AuthContextProvider>
           <ApiContextProvider>
